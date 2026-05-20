0ef24b629aa425ebc6bc62aa4ca5e736d368abd3
devcomdavi <davihmn6@gmail.com>
2026-05-20 14:56:25 -0300

    fix: imports no app.ts

--- mix-arretado-frontend/src/app/app.ts
+++ mix-arretado-frontend/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar-component/navbar-component';
+import { FooterComponent } from './components/footer-component/footer-component';
+import { HomepageComponent } from './components/homepage-component/homepage-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomepageComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
