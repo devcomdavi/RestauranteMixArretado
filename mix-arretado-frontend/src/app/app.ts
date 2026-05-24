60b72ca0f54c9adf40bdeb9f2835026b8358a261
Josef Ian <josefian2016@gmail.com>
2026-05-24 13:12:22 -0300

    criação de menu, pagina de menu, template de pratos e configuração de rotas da página de cardapio

--- mix-arretado-frontend/src/app/app.ts
+++ mix-arretado-frontend/src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar-component/navbar-component';
 import { FooterComponent } from './components/footer-component/footer-component';
-import { HomepageComponent } from './components/homepage-component/homepage-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomepageComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
