9dc42da553a151ade93c39cf3e209ff8109ca194
devcomdavi <davihmn6@gmail.com>
2026-06-14 16:58:16 -0300

    feat: adiciona estrutura inicial do projeto com Maven e configurações do Spring Boot

---
+++ mix-arretado-backend/src/main/java/com/mixarretado/mix_arretado_backend/MixArretadoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mixarretado.mix_arretado_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixArretadoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixArretadoBackendApplication.class, args);
+	}
+
+}
---
+++ mix-arretado-backend/src/test/java/com/mixarretado/mix_arretado_backend/MixArretadoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mixarretado.mix_arretado_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixArretadoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
