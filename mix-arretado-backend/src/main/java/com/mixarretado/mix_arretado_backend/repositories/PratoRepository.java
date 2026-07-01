dfc215666a1c105dee8766fd756c022c8d26d275
Josef Ian <josefian2016@gmail.com>
2026-06-30 23:08:40 -0300

    create jpa repository: findall and entitygraph

--- mix-arretado-backend/src/main/java/com/mixarretado/mix_arretado_backend/repositories/PratoRepository.java
+++ mix-arretado-backend/src/main/java/com/mixarretado/mix_arretado_backend/repositories/PratoRepository.java
@@ -4,6 +4,11 @@ import com.mixarretado.mix_arretado_backend.models.Prato;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
+import java.util.List;
+
 @Repository
 public interface PratoRepository extends JpaRepository<Prato, Long> {
+    @EntityGraph(attributePaths = {"categoria"})
+    List<Prato> findAll();
 }
